0b17a8019eab70b0dccca6a06dcb40df8649442e
SuperNG6 <sleele@gmail.com>
2021-01-21 12:31:16 +0800

    Refactoring scripts

--- root/aria2/script/stop.sh
+++ root/aria2/script/stop.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 CHECK_CORE_FILE() {
-    CORE_FILE="$(dirname $0)/script-core"
+    CORE_FILE="$(dirname $0)/core"
     if [[ -f "${CORE_FILE}" ]]; then
         . "${CORE_FILE}"
     else
-        echo "!!! script-core file does not exist !!!"
+        echo "!!! core file does not exist !!!"
         exit 1
     fi
 }
+
+CHECK_CORE_FILE
 
 GET_BASE_PATH
 COMPLETED_PATH
