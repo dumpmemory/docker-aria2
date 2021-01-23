f17af6ef748843159a528c0f8938fe87b1b91206
SuperNG6 <sleele@gmail.com>
2021-01-23 16:49:38 +0800

    update scripts

--- root/aria2/script/stop.sh
+++ root/aria2/script/stop.sh
@@ -9,15 +9,15 @@ FILE_NUM=$2
 LOAD_SETTINGS_CONF
 
 GET_BASE_PATH
-COMPLETED_PATH
-RECYCLE_PATH
-GET_PATH
 
 if [ "${RMTASK}" = "recycle" ]; then
+    RECYCLE_PATH
+    GET_PATH
     MOVE_RECYCLE
     RM_ARIA2
     exit 0
 elif [ "${RMTASK}" = "delete" ]; then
+    GET_PATH
     DELETE_FILE
     RM_ARIA2
     exit 0
